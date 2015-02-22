e089da78ae3bc4ee733ebf2fa7a6cfa5ca0fd971
Alex Ramsey <aramsey5@stanford.edu>
2015-02-22 02:34:06 -0800

    More website utility & listing info
    Implemented more listings, etc., showing more listing

--- website/app/controllers/list_controller.rb
+++ website/app/controllers/list_controller.rb
@@ -8,8 +8,27 @@ class ListController < ApplicationController
         end
 	end
 
+    def user
+        @user = User.find_by_id(params[:id])
+    end
+
     def book
         @book = Book.find_by_id(params[:id])
         @listings = @book.listings.all
     end
+
+    private
+    def condition_from_num(num)
+        if num == 1
+            return "New"
+        elsif num == 2
+            return "Like New"
+        elsif num == 3
+            return "Good"
+        elsif num == 4
+            return "Fair"
+        end
+    end
+
+    helper_method :condition_from_num
 end
